7a49b0fca0c604c6f988188eaf4bb12b69efbcf7
Juan Petter <36138796+petterjuan@users.noreply.github.com>
2025-12-14 11:00:40 -0500

    Create constants.py

---
+++ agentic_reliability_framework/memory/constants.py
@@ -0,0 +1,18 @@
+# agentic_reliability_framework/memory/constants.py
+"""
+Constants specific to memory module
+"""
+
+class MemoryConstants:
+    """Memory-specific constants"""
+    
+    # FAISS
+    FAISS_BATCH_SIZE = 10
+    FAISS_SAVE_INTERVAL_SECONDS = 30
+    VECTOR_DIM = 384
+    
+    # RAG Graph
+    MAX_INCIDENT_NODES = 1000
+    MAX_OUTCOME_NODES = 5000
+    GRAPH_CACHE_SIZE = 100
+    SIMILARITY_THRESHOLD = 0.3
