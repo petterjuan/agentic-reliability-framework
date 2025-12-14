ec66e34851c079edbc0747ebeae4d36c4a060b75
Juan Petter <36138796+petterjuan@users.noreply.github.com>
2025-12-14 13:54:20 -0500

    Update __init__.py

--- agentic_reliability_framework/engine/__init__.py
+++ agentic_reliability_framework/engine/__init__.py
@@ -4,23 +4,23 @@ Updated for v3
 """
 
 from .reliability import EnhancedReliabilityEngine, ThreadSafeEventStore
+from .v3_reliability import V3ReliabilityEngine
 from .predictive import SimplePredictiveEngine
 from .anomaly import AdvancedAnomalyDetector
 from .business import BusinessImpactCalculator, BusinessMetricsTracker
-
-# v3 components
-from .v3_reliability import V3ReliabilityEngine
 from .mcp_server import MCPServer, MCPMode, MCPRequest, MCPResponse
 from .engine_factory import EngineFactory
 
 __all__ = [
+    # v2 components
     'EnhancedReliabilityEngine',
     'ThreadSafeEventStore',
     'SimplePredictiveEngine',
     'AdvancedAnomalyDetector',
     'BusinessImpactCalculator',
     'BusinessMetricsTracker',
-    # v3 exports
+    
+    # v3 components
     'V3ReliabilityEngine',
     'MCPServer',
     'MCPMode',
