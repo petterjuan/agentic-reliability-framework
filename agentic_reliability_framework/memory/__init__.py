7733fc5abc8afad113fea442b94254fb25a49e3e
Juan Petter <36138796+petterjuan@users.noreply.github.com>
2025-12-14 13:50:29 -0500

    Update __init__.py

--- agentic_reliability_framework/memory/__init__.py
+++ agentic_reliability_framework/memory/__init__.py
@@ -1,8 +1,10 @@
 """
 Memory module for vector storage and RAG graph functionality
+Updated for v3
 """
 
 from .faiss_index import ProductionFAISSIndex
+from .enhanced_faiss import EnhancedFAISSIndex  # NEW
 from .rag_graph import RAGGraphMemory
 from .models import (
     IncidentNode, OutcomeNode, GraphEdge,
@@ -12,6 +14,7 @@ from .constants import MemoryConstants
 
 __all__ = [
     'ProductionFAISSIndex',
+    'EnhancedFAISSIndex',  # NEW
     'RAGGraphMemory',
     'IncidentNode',
     'OutcomeNode', 
